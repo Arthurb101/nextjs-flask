6c70be3ce602e1ec78132a27bb987c2393612841
Arthur buck <arthurbuck@arthurs-mbp.lan>
2024-02-22 23:41:45 -0600

    add functionality for hamburger menu

--- app/components/navbar.tsx
+++ app/components/navbar.tsx
@@ -1,24 +1,24 @@
+"use client"
 import { useState } from "react";
 
 export default function Navbar() {
 
   const [isActive, setIsActive] = useState(false);
     const onHamburgerClick = () => {
-
+      setIsActive(!isActive)
     }
-    const hamburgerClassName = 'navbar-burger ' + 
 
     return (<nav className="navbar" role="navigation" aria-label="main navigation">
     <div className="navbar-brand">
       <div className="title"> Beta </div>
-      <a role="button" onClick={onHamburgerClick} className='navbar-burger {helloword}' aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
+      <a role="button" onClick={onHamburgerClick} className={`navbar-burger ${isActive ? 'is-active' : ''}`} aria-label="menu" aria-expanded="false" data-target="navbarBasicExample">
         <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
         <span aria-hidden="true"></span>
       </a>
     </div>
     
-    <div id="navbarBasicExample" className="navbar-menu">
+    <div id="navbarBasicExample" className={`navbar-menu ${isActive ? 'is-active' : ''}`}>
       <div className="navbar-start">
         <a className="navbar-item" href="/">
           Home
